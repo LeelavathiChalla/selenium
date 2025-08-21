7355a3dfdac080b10647dfaadc324cb8e631da87
chall <chall@DESKTOP-3D6O28J>
2025-08-21 13:06:51 +0530

    Done

---
+++ eclipse-workspace/selenium_java/src/test/java/selenium_java/Selenium_12Test.java
@@ -0,0 +1,11 @@
+package selenium_java;
+
+import org.testng.annotations.Test;
+
+public class Selenium_12Test {
+
+  @Test
+  public void mainTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
